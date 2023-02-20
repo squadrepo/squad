0e08a01afc765c43b58eab5e09d3d7a9f5306357
Dom <domwhitemedia@gmail.com>
2023-02-19 23:15:10 -0500

    Simple settings screen, no functionality

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider, TextInput } from "react-native-paper";
 import { LoginScreen } from "./App/Screens/Login/login.screen";
 import { SignUpScreen } from "./App/Screens/SignUp/signup.screen";
+import { Settings } from "./App/Screens/Settings/settings.screen";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { withAuthenticator } from "aws-amplify-react-native";
@@ -15,10 +16,15 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      {/*<Stack.Navigator
         initialRouteName="Login"
         screenOptions={{ headerShown: false }}
+      >*/}
+      <Stack.Navigator
+        initialRouteName="Settings"
+        screenOptions={{ headerShown: false }}
       >
+        <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
@@ -26,4 +32,5 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+//export default withAuthenticator(App);
+export default App;
---
+++ App/Screens/Settings/settings.screen.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  TextInput,
+  Button,
+  Text,
+  Appbar,
+  Menu,
+  Divider,
+  IconButton,
+  List,
+  Switch,
+} from "react-native-paper";
+import { SafeAreaView, View, StyleSheet } from "react-native";
+
+export const Settings = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
+  return (
+    <View>
+      <Appbar.Header>
+        <Appbar.BackAction />
+        <Appbar.Content title="Settings" />
+        <Button textDecoration="underline">Save</Button>
+      </Appbar.Header>
+      <View style={styles.options}>
+        <Menu.Item title="Password" />
+        <IconButton icon="dots-horizontal" />
+      </View>
+      <View style={styles.options}>
+        <Menu.Item title="University Exclusive" />
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      </View>
+      <View style={styles.options}>
+        <Menu.Item title="Date Of Birth" />
+        <IconButton icon="dots-horizontal" />
+      </View>
+      <View style={styles.options}>
+        <Menu.Item title="Change University Email" />
+        <IconButton icon="dots-horizontal" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  options: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: 10,
+  },
+  toggle: {
+    paddingRight: 10,
+  },
+});
