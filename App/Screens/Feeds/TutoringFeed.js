1ce98244019bc9ccf0bdfb034a5bcb71cecb0833
Tapasya Sharma <t.sharm7377@gmail.com>
2023-02-27 00:18:29 -0500

    Added bottom navigation bar

---
+++ App/Screens/Feeds/TutoringFeed.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { TextInput, Button, Text } from "react-native-paper";
+import { SafeAreaView, View, StyleSheet } from "react-native";
+
+export const TutoringFeed = () => {
+  return (
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <Text style={{ fontSize: 28, color: 'black', textAlign: 'center' }}> Tutoring Feed</Text>
+  </View> 
+  );
+};
