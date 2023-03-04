dada3f177e817e3ef84e38171e2185a55da8bfe2
Dom <domwhitemedia@gmail.com>
2023-03-04 12:13:44 -0500

    merged settings and main, context added

--- App/Screens/Feeds/SocialFeed.js
+++ App/Screens/Feeds/SocialFeed.js
@@ -1,11 +1,15 @@
-import * as React from "react";
-import { TextInput, Button, Text } from "react-native-paper";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import * as React from 'react';
+import { TextInput, Button, Text } from 'react-native-paper';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { UserContext } from '../../Context';
 
 export const SocialFeed = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 28, color: 'black', textAlign: 'center' }}> Social Feed</Text>
+      <Text style={{ fontSize: 28, color: 'black', textAlign: 'center' }}>
+        {' '}
+        Social Feed
+      </Text>
     </View>
   );
 };
