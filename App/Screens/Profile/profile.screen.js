88f890c113438f0e22e7ea8e096c4ca800bc93ba
Evan Dinh <122497581+ed584@users.noreply.github.com>
2023-03-06 13:39:56 -0500

    added note

--- App/Screens/Profile/profile.screen.js
+++ App/Screens/Profile/profile.screen.js
@@ -14,6 +14,7 @@ import axios from 'axios';
 
 export const ProfileScreen = ({ navigation }) => {
 
+//Need vv this since profile screens aren't connected with home screen yet
   const {
     aboutMe,
     setAboutMe,
