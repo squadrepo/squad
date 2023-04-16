b096d71b2d2a9afb1c87bd11ceefbe0e9df191a3
Tapasya Sharma <t.sharm7377@gmail.com>
2023-04-16 18:20:42 -0400

    Added markers

--- App/Screens/Feeds/FoodFeed.js
+++ App/Screens/Feeds/FoodFeed.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { TextInput, Button, Text } from "react-native-paper";
+import { TextInput, Button, Text, Modal, Portal } from "react-native-paper";
 import { SafeAreaView, View, StyleSheet } from "react-native";
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import { BASE_API_URL } from '../../constants';
 import axios from 'axios';
 import * as Location from 'expo-location';
@@ -9,6 +9,9 @@ import * as Location from 'expo-location';
 export const FoodFeed = () => {
   const [location, setLocation] = useState();
   const [initialRegion, setInitialRegion] = useState(null)
+  const [visible, setVisible] = useState(false);
+  const [radius, setRadius] = useState(1);
+  const [markers, setMarkers] = useState(null); 
 
   useEffect(() => {
     const getPermissions = async () => {
@@ -36,9 +39,18 @@ export const FoodFeed = () => {
           longitudeDelta: 0.0421,
         })
 
-        // console.log(`${BASE_API_URL}/foodEvent?coords=${coords.latitude},${coords.longitude}&radius=5`)
-        // const response = await axios.get(`${BASE_API_URL}/foodEvent?coords=${coords.latitude},${coords.longitude}&radius=5`);
-        // console.log(response.data)
+        console.log(`${BASE_API_URL}/foodEvent?coords=${coords.latitude},${coords.longitude}&radius=${radius}`)
+        const response = await axios.get(`${BASE_API_URL}/foodEvent?coords=${coords.latitude},${coords.longitude}&radius=5`);
+        const filteredResponse = response.data.map(({ geoJson, eventName, desc, address }) => 
+        ({title: eventName,
+          description: desc,
+          address: address,   
+          coordinates : 
+          { latitude: parseFloat(geoJson.split(',')[0]),
+            longitude: parseFloat(geoJson.split(',')[1]),
+          }}));
+        setMarkers(filteredResponse)
+        console.log(filteredResponse)
 
       } catch (error) {
         console.log(error);
@@ -46,11 +58,63 @@ export const FoodFeed = () => {
     };
     getPermissions()
     getMapData()
-  });
+  }, [radius]);
 
+  const applyFilter = () => {
+    const filterRadius = radius === '' ? '1' : radius;
+    // Apply the filter with the filterRadius value
+    console.log('Applied filter with radius:', filterRadius);
+    setVisible(true);
+  };
+
+  
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={initialRegion} />
+      <MapView style={styles.map} initialRegion={initialRegion}>
+      {markers && markers.map((marker, index) => (
+        <Marker
+          key={index}
+          coordinate={marker.coordinates}
+          title={marker.title}
+          description={marker.address}
+        >
+        </Marker>
+      ))}
+        </MapView>
+      <Portal>
+        <Modal visible={visible} onDismiss={() => setVisible(false)}>
+          <View style={styles.modalContent}>
+            <TextInput
+              label="Radius (in miles)"
+              keyboardType="numeric"
+              value={radius.toString()}
+              style={{ width: 300, margin: 10 }}
+              onChangeText={(value) => {
+                if (value === '') {
+                  setRadius('');
+                } else {
+                  const parsedValue = parseFloat(value);
+                  if (!isNaN(parsedValue)) {
+                    setRadius(value);
+                  }
+                }
+              }}
+            />
+            <Button mode="contained" onPress={() => setVisible(false)}>
+              Apply
+            </Button>
+          </View>
+        </Modal>
+      </Portal>
+      <View style={styles.buttonContainer}>
+        <Button
+          mode="contained"
+          onPress={applyFilter}
+          style={styles.filterButton}
+        >
+          Filter
+        </Button>
+      </View>
     </View>
   );
 }
@@ -63,4 +127,30 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-});
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  },
+  filterButton: {
+    borderRadius: 30,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  modalTitle: {
+    fontSize:20
+  }})
