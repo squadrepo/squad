172dfc7ac57d2007a082d2a6a4d95bfdcbfc6fa6
Dom <domwhitemedia@gmail.com>
2023-05-06 12:33:03 -0400

    main update

--- App/Context.js
+++ App/Context.js
@@ -5,18 +5,18 @@ import axios from "axios";
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [aboutMe, setAboutMe] = useState('');
-  const [classHist, setClassHist] = useState('');
-  const [dispTags, setDispTags] = useState('');
+  const [aboutMe, setAboutMe] = useState("");
+  const [classHist, setClassHist] = useState("");
+  const [dispTags, setDispTags] = useState("");
   const [dobNum, setDobNum] = useState(0);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [emailLastVerifiedDateNum, setEmailLastVerifiedDateNum] = useState(0);
-  const [fullName, setFullName] = useState('');
-  const [pfpUrl, setPfpUrl] = useState('');
-  const [tags, setTags] = useState('');
-  const [tutorRating, setTutorRating] = useState('');
-  const [uid, setUid] = useState('');
-  const [univ, setUniv] = useState('');
+  const [fullName, setFullName] = useState("");
+  const [pfpUrl, setPfpUrl] = useState("");
+  const [tags, setTags] = useState("");
+  const [tutorRating, setTutorRating] = useState("");
+  const [uid, setUid] = useState("");
+  const [univ, setUniv] = useState("");
   const [univExclExp, setUnivExclExp] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
